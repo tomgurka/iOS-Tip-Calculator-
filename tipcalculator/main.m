0af3e316db64e38ffe739c88e6d78ae5f9ef1609
Tom Gurka <Tom@Toms-MacBook-Pro.local>
2014-05-28 22:15:35 -0700

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Tom Gurka on 5/28/14.
+//  Copyright (c) 2014 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
