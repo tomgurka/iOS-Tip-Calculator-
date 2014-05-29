2c00baa57f28c4bafe0a941261a845e49649a5da
Tom Gurka <tommy.gurka@gmail.com>
2014-05-29 00:43:32 -0700

    Working & Ugly
    App works!

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Tom Gurka on 5/28/14.
+//  Copyright (c) 2014 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
